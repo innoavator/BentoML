93483b4b9ff945987b31c2cd7e715cdc268490fb
Chandan Satapathy <satapathy.chandan1008@gmail.com>
2021-06-29 22:38:56 +0530

    Added Deploy method

--- bentoml/cli/bento_service.py
+++ bentoml/cli/bento_service.py
@@ -243,6 +243,72 @@ def create_bento_service_cli(
             enable_swagger=enable_swagger,
         )
 
+    # Example Usage: bentoml deploy {BUNDLE_PATH}
+    @bentoml_cli.command(
+        help="Deploy your module on AWS lambda.",
+        short_help="Deploy your endpoint on AWS.",
+    )
+    @conditional_argument(pip_installed_bundle_path is None, "bento", type=click.STRING)
+    @click.option(
+        '--yatai-url',
+        type=click.STRING,
+        default=default_yatai_url,
+        help='Remote YataiService URL. Optional. '
+             'Example: "--yatai-url http://localhost:50050"',
+    )
+    def deploy(
+            bento,
+            yatai_url,
+    ):
+        saved_bundle_path = resolve_bundle_path(
+            bento, pip_installed_bundle_path, yatai_url
+        )
+        import os
+        import uuid
+        import shutil
+        import boto3
+        import paramiko
+
+        def create_unique_name(name):
+            return ''.join([name, "-", str(uuid.uuid4())])
+
+        dir_path = saved_bundle_path
+        head, tail = os.path.split(dir_path)
+        path = shutil.make_archive(os.path.join(head, tail), 'zip', dir_path)
+        print("done zipping")
+
+        # # TODO: This part needs to be in a server of our own
+        # # # upload to s3
+        AWS_SERVER_PUBLIC_KEY = "AKIAUJMKTP5U5J2K2H4Q"
+        AWS_SERVER_SECRET_KEY = "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"
+        bucket_name = "notebook-to-prod"
+        pvt_key_path = "D:\\aws_keys\\notebook-to-prod.pem"
+        repository_name = f"lambda-{str(uuid.uuid4())[:8]}"
+        region = "eu-west-1"
+        ipv4_address = "34.244.31.222"
+        username = "ec2-user"
+
+        s3_conn = boto3.resource("s3", region_name=region, aws_access_key_id=AWS_SERVER_PUBLIC_KEY,
+                                 aws_secret_access_key=AWS_SERVER_SECRET_KEY)
+        bucket = s3_conn.Bucket(bucket_name)
+        key_path = create_unique_name(tail)
+        bucket.upload_file(path, key_path)
+        s3_uri = f"s3://{bucket_name}/{key_path}"
+        print("s3 uploaded", s3_uri)
+
+        key = paramiko.RSAKey.from_private_key_file(pvt_key_path)
+        client = paramiko.SSHClient()
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        cmd = f"python3 /home/ec2-user/automation/ec2_script.py --repository_name {repository_name} --region {region} --s3_uri {s3_uri}"
+        try:
+            client.connect(hostname=ipv4_address, username=username, pkey=key)
+            stdin, stdout, stderr = client.exec_command(cmd)
+            res = stdout.read()
+            print(res.decode())
+            client.close()
+        except Exception as e:
+            print(e)
+
     # Example Usage: bentoml save-direct --path={PATH}
     @bentoml_cli.command(
         help="Generate and save the entire module from a python file",
